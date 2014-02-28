e3448e02516b86608964af858a94205f33c890ec
mariano gigli <mariano.gigli@gmail.com>
2014-02-28 20:22:40 -0300

    added remove and router functionality

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,3 +1,66 @@
+/* Router */
+function Router(){
+    this.routes = {};
+};
+
+Router.prototype = {
+    navigate: function(extra_params) {
+        var uri = decodeURIComponent(window.location.hash);
+        var params = this.parse_uri(uri);
+        var url_hash = '';
+        for (var attr in extra_params) {
+            params[attr] = extra_params[attr];
+        }
+        for (key in params) {
+            if(params[key] && key != "limit"){
+                url_hash += '/' + key + '/' + params[key];
+            }
+        }
+        window.location.hash = url_hash;
+    },
+    parse_uri: function(uri){
+        var patern = {
+            keywords: /keywords\/([^\/&]+)/,
+            sort_on: /sort_on\/(created|price|score)/,
+            sort_order: /sort_order\/(up|down)/,
+            min_price: /min_price\/([0-9]+)/,
+            max_price: /max_price\/([0-9]+)/,
+            page: /page\/([0-9]+)/
+        };
+        var extra_params = {};
+        if (typeof uri.match(patern.keywords) !== 'undefined' && uri.match(patern.keywords) !== null){
+            extra_params.keywords = uri.match(patern.keywords)[1];
+        }
+        if (typeof uri.match(patern.sort_on) !== 'undefined' && uri.match(patern.sort_on) !== null){
+            extra_params.sort_on = uri.match(patern.sort_on)[1];
+        }
+        if (typeof uri.match(patern.sort_order) !== 'undefined' && uri.match(patern.sort_order) !== null){
+            extra_params.sort_order = uri.match(patern.sort_order)[1];
+        }
+        if (typeof uri.match(patern.price) !== 'undefined' && uri.match(patern.price) !== null){
+            extra_params.price = uri.match(patern.price)[1];
+        }
+        if (typeof uri.match(patern.min_price) !== 'undefined' && uri.match(patern.min_price) !== null){
+            extra_params.min_price = uri.match(patern.min_price)[1];
+        }
+        if (typeof uri.match(patern.max_price) !== 'undefined' && uri.match(patern.max_price) !== null){
+            extra_params.max_price = uri.match(patern.max_price)[1];
+        }
+        if (typeof uri.match(patern.page) !== 'undefined' && uri.match(patern.page) !== null){
+            extra_params.page = uri.match(patern.page)[1];
+        }
+        return extra_params;
+    },
+    route: function () {
+        var hash = decodeURIComponent(window.location.hash) || '';
+        if (typeof this.router.routes[hash] === 'undefined' || this.router.routes[hash] === null){
+            this.router.routes[hash] = this.router.parse_uri(hash);
+        }
+        this.api.fetch(this.query_params(this.router.routes[hash]), this.parse.bind(this));
+    }
+};
+
+
 /* API */
 function Api(api_key, url){
     this.api_key = (api_key)? api_key : 'kd863bvbqpbk67beub60pi5p';
@@ -8,7 +71,7 @@ function Api(api_key, url){
 Api.prototype = {
     fetch: function (params, callback) {
         this.callback = callback;
-        var url_params = '&callback=api.callback&includes=Images(url_570xN):1:0';
+        var url_params = '&callback=app.api.callback&includes=Images(url_570xN):1:0';
         for (key in params) {
             if(params[key]){
                 url_params += '&' + key + '=' + params[key];
@@ -21,7 +84,7 @@ Api.prototype = {
     },
     callback: function(){}
 };
-var api = new Api();
+
 
 /* Item */
 function Item (data){
@@ -52,6 +115,7 @@ Item.prototype = {
     }
 };
 
+
 /* Generic Views */
 function Views(params){
     this.element = (params.element)? params.element : 'div';
@@ -68,6 +132,7 @@ Views.prototype = {
         this.elem = elem;
     }
 };
+
 
 /* Paginartor */
 function Paginator (data){
@@ -124,53 +189,66 @@ Paginator.prototype = {
     }
 };
 
+
 /* Main application */
 function App(form, results, paginator_id, api_key, api_url){
+    this.router = new Router();
     this.api = new Api(api_key, api_url);
     this.form = document.getElementById(form);
     this.results = document.getElementById(results);
     this.paginator_id = paginator_id;
-    this.query_set = {};
-    this.loading = false;
-    this.bind_events();
+    this.bind_event(this.form,'submit', this.submit.bind(this), false);
+    this.bind_event(window, 'hashchange', this.router.route.bind(this), false);
+    this.bind_event(window, 'load', this.router.route.bind(this), false);
 }
 
 App.prototype = {
-    bind_events: function (target, event, func, capture) {
+    bind_event: function (target, event, func, capture) {
         if(typeof target !== "undefined" && target !== null){
             target.addEventListener(event, func, capture);
         }
-        this.form.addEventListener('submit', this.submit.bind(this), false);
+    },
+    bind_remove_elements: function () {
+        var bind_remove_elem = document.getElementsByClassName('view-more delete');
+        for(var x=0; x < bind_remove_elem.length; x++){
+            this.bind_event(bind_remove_elem[x], 'click', this.set_exclude_item.bind(this), false);
+        }
     },
     query_params: function (extra_params) {
-        var price = this.form.price.value.split(';');
+        var keywords = (extra_params.keywords)? extra_params.keywords : this.form.query.value;
+        var sort_on = (extra_params.sort_on)? extra_params.sort_on : this.form.order.value;
+        var sort_order = (extra_params.sort_order)? extra_params.sort_order : this.form.direction.value;
+        var price = (extra_params.price)? extra_params.price.split(';') : this.form.price.value.split(';');
+        var min_price = (extra_params.min_price)? extra_params.min_price : price[0];
+        var max_price = (extra_params.max_price)? extra_params.max_price : price[1];
         var page = (extra_params.page)? extra_params.page : 1;
         this.params = {
-            keywords: this.form.query.value,
-            sort_on: this.form.order.value,
-            sort_order: this.form.direction.value,
-            min_price: price[0],
-            max_price: price[1],
+            keywords: keywords,
+            sort_on: sort_on,
+            sort_order: sort_order,
+            min_price: (min_price)? parseFloat(min_price) : min_price,
+            max_price: (max_price)? parseFloat(max_price) : max_price,
             page: parseInt(page),
             limit: 10
         };
         return this.params;
     },
     submit: function (event) {
-        api.fetch(this.query_params({page: 1}), this.parse.bind(this));
+        this.router.navigate(this.query_params({}));
         event.preventDefault();
-        return false;
     },
     parse: function (data) {
-//        var _this = this;
         if (parseInt(data.params.page) === parseInt(this.params.page)) {
             this.clear_results();
         }
+        data = this.exclude_items(data);
         this.results.appendChild(this.parse_items(data.results));
+        this.bind_remove_elements();
         data.pagination.count = data.count;
         this.results.appendChild(this.paginator(data.pagination));
         this.paginator_el = document.getElementById(this.paginator_id);
-        this.bind_events(this.paginator_el, 'click', this.get_page.bind(this), false);
+        this.bind_event(this.paginator_el, 'click', this.get_page.bind(this), false);
+
     },
     parse_items: function (results) {
         var buffer = document.createDocumentFragment();
@@ -203,6 +281,34 @@ App.prototype = {
         message_view.render(item);
         buffer.appendChild(message_view.elem);
     },
+    exclude_items: function (data) {
+        data.results = data.results.filter(this.remove_item);
+        return data;
+    },
+    remove_item: function (item){
+        var excludes_items = localStorage.getItem('excludes_items');
+        if(excludes_items !== null){
+            excludes_items = excludes_items.split(',');
+            for(var x=0; x < excludes_items.length; x++){
+                if (item.listing_id == excludes_items[x]){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return true;
+    },
+    set_exclude_item: function (event) {
+        var current_content = window.localStorage.getItem('excludes_items');
+        var listing_id = event.target.dataset.id;
+        if(current_content === null){
+            window.localStorage.setItem('excludes_items',listing_id);
+        }else{
+            window.localStorage.setItem('excludes_items',current_content+','+listing_id);
+        }
+        event.toElement.parentNode.parentNode.parentNode.className = " item-result-removed";
+        event.preventDefault();
+    },
     paginator: function (data) {
         var buffer = document.createDocumentFragment();
         var paginator;
@@ -226,8 +332,7 @@ App.prototype = {
         page = {
             page: event.target.dataset.page
         };
-        api.fetch(this.query_params(page), this.parse.bind(this));
+        this.router.navigate(page);
         event.preventDefault();
-        return false;
     }
 };
